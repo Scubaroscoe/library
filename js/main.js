ca7cc27461b7dfb57fc5d39203e42cb721a72666
Alexander Konefal <zanderkonefal@gmail.com>
2021-09-13 13:39:06 -0400

    Got table up. Still working on book adding button

--- js/main.js
+++ js/main.js
@@ -16,7 +16,7 @@ function Book(title, author, pages, read) {
 }
 
 let library = [];
-function addBookToLibrary(title, author, pages, read) {
+function addNewBookToLibrary(title, author, pages, read) {
 	//take users input and store new book objects into an array that holds all book objects
 	// let title = prompt("Book title: ");
 	// let author = prompt("Book author: ");
@@ -26,14 +26,123 @@ function addBookToLibrary(title, author, pages, read) {
 	library.push(new Book(title, author, pages, read));
 }
 
+function addBookToLibrary(book) {
+	library.push(book);
+}
+
+function capitalize(someStr) {
+	let returnStr = someStr[0].toUpperCase();
+	returnStr += someStr.slice(1);
+	return returnStr;
+}
+
+// create just the header for the table
+function createTableHead(table, someBook) {
+	// const tblHead = document.createElement('thead');
+	let tblHead = table.createTHead();
+	let tblHeadRow = tblHead.insertRow();
+
+	for (let prop of Object.keys(someBook)) {
+		if (prop === 'info') {
+			// skip for now
+		} else {
+			let th = document.createElement('th');
+			let text = document.createTextNode(capitalize(prop));
+			th.appendChild(text);
+			tblHeadRow.appendChild(th);
+		}
+	}
+}
+
+// create all cells
+function createTableRows(body, library) {
+	for (let i = 0; i < library.length; i++){
+		//create a table row
+		const row = document.createElement("tr");
+
+		for (let j in library[i]) {
+			// create td element and text node. text node fills the td
+			if (j === 'info'){
+				//skip for now
+			} else {
+				let cell = document.createElement('td');
+				let cellText = document.createTextNode(`${library[i][j]}`);
+				cell.appendChild(cellText);
+				row.appendChild(cell);
+			}
+		}
+		body.appendChild(row);
+	}
+}
+
 const bookContainer = document.querySelector('#book-container');
 function displayCatalogue() {
-	for (let i = 0; i < library.length; i++){
-		// create cards with book details and add them to the book container
-	}
+	
+	// create a table element and a tbody element
+	const tbl = document.createElement('table');
+
+	createTableHead(tbl, bloodMusic);
+
+	const tblBody = document.createElement('tbody');
+
+	createTableRows(tblBody, library);
+
+	tbl.appendChild(tblBody);
+	bookContainer.appendChild(tbl);
 }
 
 
 
 let bloodMusic = new Book("Blood Music", "Greg Bear", 295, true);
-console.log(bloodMusic.info());
+let travelsWithCharlie = new Book("Travels with Charlie", "John Steinbeck", 274, true);
+let unsocialSocialist = new Book("The Unsocial Socialist", "George Bernard Shaw", 389, true);
+// console.log(bloodMusic.info());
+
+addBookToLibrary(bloodMusic);
+addBookToLibrary(travelsWithCharlie);
+addBookToLibrary(unsocialSocialist);
+
+displayCatalogue();
+
+// button related logic comes next
+const buttonNewBook = document.querySelector('#new-book');
+
+buttonNewBook.addEventListener('click', (e) => {
+	let form = document.createElement("form");
+	form.setAttribute("method", "post");
+	form.setAttribute("action", "submit.php");
+
+	let title = document.createElement('input');
+	title.setAttribute('type', 'text');
+	title.setAttribute('title', 'title');
+	title.setAttribute('placeholder', 'Title');
+
+	let author = document.createElement('input');
+	author.setAttribute('type', 'text');
+	author.setAttribute('author', 'author');
+	author.setAttribute('placeholder', 'Author');
+
+	let pages = document.createElement('input');
+	pages.setAttribute('type', 'text');
+	pages.setAttribute('pages', 'pages');
+	pages.setAttribute('placeholder', 'Pages');
+
+	let read = document.createElement('input');
+	read.setAttribute('type', 'text');
+	read.setAttribute('read', 'read');
+	read.setAttribute('placeholder', 'read');
+
+	// create a submit button
+	let submitButton = document.createElement("input");
+	submitButton.setAttribute("type", "submit");
+	submitButton.setAttribute("value", "Submit");
+
+	// append inputs to the form
+	form.appendChild(title);
+	form.appendChild(author);
+	form.appendChild(pages);
+	form.appendChild(read);
+	form.appendChild(submitButton);
+
+	
+});
