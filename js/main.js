5e6ba2da44e50030a91666dd336c4897d40218bb
Alexander Konefal <zanderkonefal@gmail.com>
2021-09-14 14:54:40 -0400

    largely finished project. It does everything required. Solution for losing functions from objects in localStorage is ugly, but it works.

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+// This function checks if browser can use webstorage API. Only really old browsers would not work
 function storageAvailable(type) {
 	var storage;
 	try {
@@ -32,33 +33,53 @@ function Book(title, author, pages, read) {
 	this.pages = pages;
 	this.read = read;
 
-	this.info = function() {
-		let readOutput;
-		if (read) {
-			readOutput = "have read.";
-		} else {
-			readOutput = "not read yet.";
-		}
-		return `${title} by ${author}, ${pages} pages, ${readOutput}`;
-	}
+	// this.info = function() {
+	// 	let readOutput;
+	// 	if (read) {
+	// 		readOutput = "have read.";
+	// 	} else {
+	// 		readOutput = "not read yet.";
+	// 	}
+	// 	return `${title} by ${author}, ${pages} pages, ${readOutput}`;
+	// }
 
-	this.toggle = function() {
-		if (this.read) {
-			this.read = false;
-		} else {
-			this.read = true;
-		}
+	// this.toggle = function() {
+	// 	if (this.read) {
+	// 		this.read = false;
+	// 	} else {
+	// 		this.read = true;
+	// 	}
+	// }
+}
+
+function toggle(book) {
+	if (eval(book.read)) {
+		// book.read = false;
+		return "false";
+	} else {
+		// book.read = true;
+		return "true";
 	}
 }
 
-let library = [];
+function info(book) {
+	let readOutput;
+	if (read) {
+		readOutput = "have read.";
+	} else {
+		readOutput = "not read yet.";
+	}
+	return `${title} by ${author}, ${pages} pages, ${readOutput}`;
+}
 
 function addNewBookToLibrary(title, author, pages, read) {
 	library.push(new Book(title, author, pages, read));
+	populateStorage();
 }
 
 function addBookToLibrary(book) {
 	library.push(book);
+	populateStorage();
 }
 
 function capitalize(someStr) {
@@ -74,14 +95,22 @@ function createTableHead(table, someBook) {
 
 	for (let prop of Object.keys(someBook)) {
 		let th = document.createElement('th');
+		let text = document.createTextNode(capitalize(prop));
+		th.appendChild(text);
+		tblHeadRow.appendChild(th);
+		// tblHeadRow.appendChild(document.createElement('th').appendChild(document.createTextNode(capitalize(prop)))); // can be done in one line
+	}
+	// This is hideous, but localStorage was having significant problems with storing 
+	// the functions defined in book, so I made them functions outside of the Book
+	// object. This part is an ugly fix to add the two columns previously assumed to exist
+	// in the book object.
+	for (let i = 0; i < 2; i++) {
+		let th = document.createElement('th');
 		let text;
-		if (prop === 'info') {
-			// skip for now
+		if (i === 0) {
 			text = document.createTextNode("Remove");
-		} else if (prop === 'toggle') {
-			text = document.createTextNode("Toggle Read");
 		} else {
-			text = document.createTextNode(capitalize(prop));
+			text = document.createTextNode("Toggle Read");
 		}
 		th.appendChild(text);
 		tblHeadRow.appendChild(th);
@@ -89,7 +118,9 @@ function createTableHead(table, someBook) {
 }
 
 // create all cells
-function createTableRows(body, library) {
+// function createTableRows(body, library) {
+	function createTableRows(body) {	
+	// if (library) {	// This needed in case library is set to null by setStorage
 	for (let i = 0; i < library.length; i++){
 		//create a table row
 		const row = document.createElement("tr");
@@ -97,32 +128,43 @@ function createTableRows(body, library) {
 		for (let j in library[i]) {
 			// create td element and text node. text node fills the td
 			let cell = document.createElement('td');
+			let cellCont = document.createTextNode(`${library[i][j]}`);
+			cell.appendChild(cellCont);
+			row.appendChild(cell);
+		}
+		// This is hideous, but localStorage was having significant problems with storing 
+		// the functions defined in book, so I made them functions outside of the Book
+		// object. This part is an ugly fix to add the two columns previously assumed to exist
+		// in the book object.
+		for (let k = 0; k < 2; k++) {
+			let cell = document.createElement('td');
 			let cellCont;
-			if (j === 'info'){
-				//skip for now
+			if (k === 0) {
 				cellCont = document.createElement('button');
 				cellCont.textContent = "Remove";
 				cellCont.addEventListener('click', (e) => {
 					let parentRow = document.querySelector(`tr[data-ind="${i}"]`);
 					parentRow.remove();
 					delete library[i];
+					populateStorage();	// This to keep stored library perfectly updated
 				})
-			} else if (j === 'toggle') {
+			} else {
 				cellCont = document.createElement('button');
 				cellCont.textContent = "Toggle Read";
 				cellCont.addEventListener('click', (e) => {
-					library[i].toggle();
+					library[i].read = toggle(library[i]);
 					let parentRow = document.querySelector(`tr[data-ind="${i}"]`);
 					let rowCells = parentRow.querySelectorAll('td');
-					for (let prop in rowCells) {
-						let propVal = rowCells[prop].textContent;
-						if (propVal === "true" || propVal == "false") {
-							rowCells[prop].textContent = library[i].read;
-						}
+
+					let propVal = rowCells[3].textContent; // This should always be 3
+					if (propVal === "true" || propVal == "false") {
+						rowCells[3].textContent = library[i].read;
 					}
+
+					// for (let prop in rowCells) {
+					// }
+					populateStorage();	// This to keep stored library perfectly updated
 				});
-			} else {
-				cellCont = document.createTextNode(`${library[i][j]}`);
 			}
 			cell.appendChild(cellCont);
 			row.appendChild(cell);
@@ -131,15 +173,22 @@ function createTableRows(body, library) {
 	}
 }
 
-function populateStorage() {	// needs work...
-	console.log(`populating storage with this: ${document.querySelector('#book-container')}`)
-	localStorage.setItem('book-container', document.querySelector('#book-container'));
+function populateStorage() {
+	library = library.filter(n => n);
+	console.log(`populating storage with this: ${JSON.stringify(library)}`);
+	localStorage.setItem('library', JSON.stringify(library));
 	setStorage();
 }
 
 function setStorage() {
-	var storedContainer = localStorage.getItem('book-container');
-	document.querySelector('#book-container').value = storedContainer;
+	let storedContainer = JSON.parse(localStorage.getItem('library'));
+	if (storedContainer) {	// if storedContainer is not null, then store in library
+		library = storedContainer;
+	} else {	// else set library to empty array
+		library = [];
+	}
+	// library = [];
+	// localStorage.clear();
 }
 
 const bookContainer = document.querySelector('#book-container');
@@ -161,7 +210,16 @@ function displayCatalogue() {
 	tbl.appendChild(tblBody);
 	bookContainer.appendChild(tbl);
 	// JSON.stringify
+	// populateStorage();
+}
+
+let library = [];
+if (!localStorage.getItem('library')) {
+	// populate local storage
 	populateStorage();
+} else {
+	// add the existing stored values to the page where they belong
+	setStorage();
 }
 
 // let bloodMusic = new Book("Blood Music", "Greg Bear", 295, true);
@@ -172,7 +230,6 @@ function displayCatalogue() {
 // addBookToLibrary(bloodMusic);
 // addBookToLibrary(travelsWithCharlie);
 // addBookToLibrary(unsocialSocialist);
-
 displayCatalogue();
 
 // button related logic comes next
@@ -232,18 +289,11 @@ buttonNewBook.addEventListener('click', (e) => {
 	newBookContainer.appendChild(form);
 });
 
-if (!localStorage.getItem('book-container')) {
-	// populate local storage
-	populateStorage();
-} else {
-	// add the existing stored values to the page where they belong
-	setStorage();
-}
-
-window.addEventListener('storage', function(e) {
-	document.querySelector('.my-key').textContent = e.key;
-	document.querySelector('.my-old').textContent = e.oldValue;
-	document.querySelector('.my-new').textContent = e.newValue;
-	document.querySelector('.my-url').textContent = e.url;
-	document.querySelector('.my-storage').textContent = e.storageArea;	
-});
+// You would use this if you had multiple pages
+// window.addEventListener('storage', function(e) {
+// 	document.querySelector('.my-key').textContent = e.key;
+// 	document.querySelector('.my-old').textContent = e.oldValue;
+// 	document.querySelector('.my-new').textContent = e.newValue;
+// 	document.querySelector('.my-url').textContent = e.url;
+// 	document.querySelector('.my-storage').textContent = e.storageArea;	
+// });
